33d203428c960d31231f41365f8efdea90b40cba
Will James <w.james1497@gmail.com>
2023-05-27 17:35:41 +0100

    update title and favicon

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { CountdownTimer } from './CountdownTimer';
 
 function App() {
 
-  const WED_DATE = new Date("2023-10-21 13:30:00");
+  const DATE = new Date("2023-10-21 13:30:00"); 
 
   return (
     <div className='show-counter' >
-      <CountdownTimer targetDate={WED_DATE}/>
+      <CountdownTimer targetDate={DATE}/>
     </div>
   );
 }
